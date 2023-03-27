dff1d8a59b44f530c90431ab62eeebbc0ee4b73d
Pedro Costa <phmc99@outlook.com>
2023-03-26 22:11:34 -0300

    first commit

---
+++ src/config/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  "development": {
+    "username": "user",
+    "password": "password",
+    "database": "db",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
+}
